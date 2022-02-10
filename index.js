cd84188ac64aff050b1efd52be392bfa8a928e03
Itcow <cowcerit@alumni.vcu.edu>
2022-02-09 19:48:36 -0500

    finished product

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your Code Here
+let userName = window.prompt ('Enter your name');
+let userScore = 0
+let playAgain = true
+
+
+function playGame(){
+    for(let i=0; i < questions.length; i++){
+    let question = questions[i]
+    window.prompt(question.text);
+    let userAnswer = window.prompt(question.text)
+    if (userAnswer === question.correctAnswer){
+        console.log('Correct')
+
+        userScore = userScore + 10
+    
+
+    }window.alert(userName+"'s user score is " + userScore);
+}
+}
+while(playAgain === true){
+    playGame()
+    let userChoice = window.prompt('Would you like to play again? Say "yes" or "no".')
+    if(userChoice === "yes"){playAgain = true}
+    else {playAgain = false
+    window.alert("thank you, " +userName +", for playing!");
+}
+}
+
